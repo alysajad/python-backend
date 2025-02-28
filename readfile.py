6ef052ef43908b0e912598ddecdb30779a3d98e3
SAJAD HUSSAIN <huxi1314k@gmail.com>
2025-02-28 18:51:50 +0530

    readfile.py

---
+++ readfile.py
@@ -0,0 +1,15 @@
+'''count_file = open('countries.txt','r') #a append
+
+open('countries.txt','r') #r read
+
+
+print(count_file.readlines())
+print(line)
+
+count_file.close()
+# The code
+#  above reads the content of a file named countries.txt and prints it to the console.
+'''
+
+count_file = open('country.txt','a') #a append
+count_file.write('new world\n')
