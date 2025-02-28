e98122c0f5a3b067c22bb61b400a329d0a449ac0
SAJAD HUSSAIN <huxi1314k@gmail.com>
2025-02-28 19:19:24 +0530

    inherit.py

---
+++ inherit.py
@@ -0,0 +1,9 @@
+# should be in same directory
+
+from new import Student
+
+class person(Student):
+    pass
+p1=person
+print(p1.name)
+print(p1.age)
---
+++ new.py
@@ -0,0 +1,5 @@
+class Student():
+    name='tim'
+    age=23
+    gender='male'
+    standard='12th'
