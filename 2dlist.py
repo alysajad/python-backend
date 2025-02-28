de4724599036ef2dc4b1bbf0750b1018964672ee
SAJAD HUSSAIN <huxi1314k@gmail.com>
2025-02-28 18:11:24 +0530

    2dlist.py

---
+++ 2dlist.py
@@ -0,0 +1,16 @@
+my_list = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+#list in a list
+
+print (my_list[1][2]) #output: 6
+
+for list in my_list:
+    for row in list:
+        print(row)
+#output: 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+#python work in the indentation
